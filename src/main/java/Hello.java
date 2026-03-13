4243cad85149aa83bdf74add126796cabe08c07d
kimdonghyeon0107 <tonyhyeon2007@gmail.com>
2026-03-13 15:07:52 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+
+     /*
+     * public static void main(sring[] args 함수는 자바에서 가장 먼저실행되는 함수
+     *
+     * 즉, 자바 프로그램의 시작이 되는 함수이며, 반드시 Main 함수가 있어야지 자바가 실행가능함
+     * Main 함수가 없으면 실행이 안됨
+     * */
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("I want go home");
+        System.out.println("My name is Kim Dong Hyeon");
+        System.out.println("My dog's name is Teayang, and he is 12 years old");
+        System.out.println("My hobbies are soccer and gamees")
+        System.out.println("I attend the Big Data Software Department at a polytechnic college")
+    }
+}
